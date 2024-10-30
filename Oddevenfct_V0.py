4ba81384a10e3fcbe2698d0b36e53bf4a98b5e14
JesterV13 <143851627+JesterV13@users.noreply.github.com>
2024-10-30 10:53:30 -0400

    Update and rename Oddevenfct_V0.py to OddorEven.py

--- Oddevenfct_V0.py
+++
@@ -1,8 +0,0 @@
-# Odd or Even Checker
-print("Enter a number:")
-num = int(input())
-
-if int(num % 2 == 0):
-    print("The number", num, "is Even!")
-else:
-    print("The number", num,  "is Odd!")
