75e615cb53750d3b060d425414c7c6d9738de43c
JesterV13 <143851627+JesterV13@users.noreply.github.com>
2024-10-30 10:54:59 -0400

    Update OddorEven.py

--- OddorEven.py
+++ OddorEven.py
@@ -1,27 +1,33 @@
 # Odd or Even Checker as Function
-def OddorEven(num):
-    if int(num % 2 == 0):
+def odd_even_checker(num):
+    if (num % 2 == 0):
         print("The number", num, "is Even!")
         # math check for odd
-    elif int(num % 2 == 1):
+    elif (num % 2 == 1):
         print("The number", num, "is Odd!")
 
+# added exit function
+def exit_program():
+    print("Farewell...")
+    exit()
 
-# added greeting to app
+# greeting to app
+# added additional greeting for program exit
 print("Welcome to the ODD or EVEN app")
+print('If you would like to exit at anytime, type "Stop".\n')
 print("Please Enter a Number:")
 
-# added a while loop to run the program until user ended
-i = 1
-while i == 1:
+# added a while "true" loop to run the program until ended
+while True:
     # check for string
     try:
-        OddorEven(int(input()))
-
-        # user input to end program
-        user_input = input("Would you like to exit the program? (y/n): ")
-        if user_input.lower() == "y":
-            i = i - 1
+        # added exit call key and moved input to its own variable
+        num = input()
+        if num.lower() == "stop":
+            exit_program()
+        else:
+            num = int(num)
+            odd_even_checker(num)
 
     except ValueError:
-        print("Enter a number only!")
+        print("Enter a Number only!")
