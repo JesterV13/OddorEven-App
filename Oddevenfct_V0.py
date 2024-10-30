b05ddad89d153aafe43c30a3db3adbae158065ac
JesterV13 <143851627+JesterV13@users.noreply.github.com>
2024-10-30 10:22:15 -0400

    Add files via upload

---
+++ Oddevenfct_V0.py
@@ -0,0 +1,8 @@
+# Odd or Even Checker
+print("Enter a number:")
+num = int(input())
+
+if int(num % 2 == 0):
+    print("The number", num, "is Even!")
+else:
+    print("The number", num,  "is Odd!")
